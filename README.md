c7efb49a6269099066d04a0f964f22d405e60906
MitchyT <42443446+MitchyT@users.noreply.github.com>
2018-11-27 22:24:45 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,242 @@
-# PrivateBlockChain
-Udacity Project Building Your Own Private Blockchain
+# Blockchain Data
+
+Blockchain has the potential to change the way that the world approaches data. Develop Blockchain skills by understanding the data model behind Blockchain by developing your own simplified private blockchain.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+Installing Node and NPM is pretty straightforward using the installer package available from the (Node.js® web site)[https://nodejs.org/en/].
+
+### Configuring your project
+
+- Use NPM to initialize your project and create package.json to store project dependencies.
+```
+npm init
+```
+- Install crypto-js with --save flag to save dependency to our package.json file
+```
+npm install crypto-js --save
+```
+- Install level with --save flag
+```
+npm install level --save
+```
+#### Web Server
+
+<b>This type of web server should only be used and accessed locally (127.0.0.1||localhost).</b> 
+
+- Install fs 
+```
+npm install fs
+```
+- Install express
+```
+npm install express
+```
+- Install bodyParser 
+```
+npm install body-parser
+```
+
+
+## Files/Folders
+```
+project2-----|
+             |----levelSandbox.js
+             |
+             |----simpleChain.js
+             |
+             |----app.js
+             |
+             |----privateblockchainUI.html
+             |
+             |----chaindata 
+                      |
+                      |---files to store the blockchain (NOT ADVISED TO CHANGE INSIDE CHAINDATA)
+             |
+             |----README.md
+
+```          
+
+#### levelSandbox.js
+
+Contains leveldb class, this allows blocks on the chain to be persistent. The functions within the class are asynchronous meaning multiple operations can occur at once. Asynchronous operations are done through <a href = "https://developers.google.com/web/fundamentals/primers/promises">Promises </a> witch have to be dealt with appropriately.
+
+#### simpleChain.js
+
+Consist of two classes, Block class and Blockchain class. Together creates a functional private blockchain. With the help of levelSandbox the block chain can be saved and reused.
+
+Block class consist of only a constructor that describes the block object (hash,height,body,time,previousBlockHash).
+
+BlockChain class contains asynchronous functions that handle the creation of the chain and the management of blocks on the chain. The reason for Blockchain functions to return Promise it allows the data from leveldb to be used without having unknown variables.
+ 
+<u>NOTE</u>
+I find this file to be good for building appilcations as shown in app.js
+
+#### app.js
+
+Contains the same code in simpleChain but, added a function web server that will accept get and post request from the client browser to manage and view the blockchain. Each get and post request calls out corresponding blockChain function with variables passed from the client. It utilizes the await operator to receive the Promise that will be sent back to the client.
+
+#### privateblockchainUI.html
+
+Privateblock chain UI
+Basic html code and dynamic javascript to better visualize and manage the private block chain. 
+
+#### chaindata
+
+Blockchain data is stored. If a block is changed or a new chain is needed, chaindata folder can be deleted.
+
+
+#### README.md
+
+Your reading it!
+
+### Code Flow
+```
+Blockchain class                      leveldb class
+    |                                    |
+    |--addBlock()                        |-- addLevelDBData()
+    |                                    |
+    |--validateBlock()                   |-- changeDBDate()
+    |                                    |
+    |--validateChain()                   |-- getLevelDBData()
+    |                                    |
+    |--getHeight()                       |-- addDataToLevelDB()
+    |                                    |
+    |--getBlock()                        |-- getBlockCount()
+                                         |
+                                         |-- getChain()
+```
+
+
+#### Creating blocks/Genesis block and saving to leveldb 
+
+ 
+A check is done in the constructor of the BlockChain class that Promises to get the height of the saved chain. Using a leveldb function called getBlockCount() line[82 - 95] if nothing is returned a genesis block is created. Else we use the height to create a new block with the height of the last block created.
+
+
+Using BlockCreator function to create new blocks line[292 - 310]. A timing and interval function that creates a defined amount of new block, that is passed to addBlock within in BlockChain class. addBlocks will then set block height, time, previousBlockhash and then a hash of the block itself. This is then passed to addDataToLevelDB within leveldb class to be saved. 
+
+```
+BlockCreator()
+      |
+      |--addBlock()
+            |
+            |-- addDataToLevelDB()
+      
+```
+
+#### Validate Block / Validate Chain
+
+Validating a block is done in validateBlock in the BlockChain class.
+First requesting a block by its height with getLevelDBData. The next step is to rehash the block to compare the hash in the block. If the hashes are not equal then the block has been change therefore invalid.
+
+
+```
+validateBlock()
+      |
+      |--getLevelDBData()
+```
+
+Validating the chain starts by requesting the height of the chain getBlockCount in leveldb. Then looping the the range of the blocks with getLevelDBData and comparing two things. First the last blocks hash with the current blocks previous hash and like validating a single block, rehashing the block. Only if both are true the chain is valid.
+
+```
+validateChain()
+      |
+      |--getBlocksCount()
+     then
+      |
+      |--getLevelDBData() 
+```
+
+#### get Block / chain height
+
+
+getHeight function found in BlockChain Class utilizes getBlocksCount to return the height of the chain. 
+
+```
+getHeight()
+    |
+    |--getBlocksCount()
+```
+getBlock function found in BlockChain class uses getLevelDBData to return a specific block.
+
+```
+getBlock()
+    |
+    |--getLevelDBData()
+```
+
+### Managing the Private Blockchain 
+
+
+#### simpleChain.js
+
+To use simpleChain.js to manage the blockchain use lines 313-328 for creating and validation.
+
+First call out BlockCreator to generate some blocks. The two inputs are timing between block being created and amount of blocks to be created. 
+Recommend to stay above 1 sec per block (1 sec == 1000)
+<u>NOTE</u> this will generate chaindata folder.
+```
+// Used to create blocks
+BlockCreator(1000,5);
+```
+After creating blocks the functions below are available. 
+```
+//validateChain
+PrivateChain.validateChain();
+```
+Input: block you want to validate.
+```
+//validateBlock
+PrivateChain.validateBlock(1);
+```
+```
+//Get height of chain
+PrivateChain.getHeight();
+```
+Input: block you want to inspect.
+```
+//Get single Block
+PrivateChain.getBlock(1);
+```
+
+#### app.js
+
+Starting the web server
+first cd into project repository ``` cd Project_2 ```
+run 
+```
+node app.js
+```
+The server will be listing on 8080 for a client connection.
+
+<u>Developed with Google Chrome</u>
+Copy and paste the socket into url of browser  ```http://127.0.0.1:8080```
+
+
+
+
+Having trouble on windows, windows firewall might need to be turn off.
+
+Copy and paste into control panel 
+```Control Panel\System and Security\Windows Defender Firewall\Customize Settings``` then select ```Turn off windows defender Firewall ```. Dont forget to turn it back on!
+
+The video below shows the UI in action, from creating blocks to getting a better visual on how blocks are validated and connected. 
+
+<video width= '940' height = '480' controls>
+<source  src = "UI.mp4" type = "video/mp4">
+</video>
+
+
+
+
+
+### Potential Short Comings
+
+If a large amount of blocks where to be created memory would eventually be filled. To solve this either set a limit on how many blocks can be in memory. Or have no blocks in memory, they would be directly saved to the database.
+
+
---
+++ app.js
@@ -0,0 +1,449 @@
+
+/* ===== SHA256 with Crypto-js ===============================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
+|  =========================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+
+// leveldb import 
+const Leveldb = require('./levelSandbox.js');
+
+// Constuct leveldb object (Saved blockchain)
+const db = new Leveldb.leveldb;
+
+// web server imports 
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app   = express();
+
+/* ===== Block Class ==============================
+|  Class with a constructor for block 			   |
+|  ===============================================*/
+
+class Block{
+	constructor(data){
+     this.hash = "",
+     this.height = 0,
+     this.body = data,
+     this.time = 0,
+     this.previousBlockHash = ""
+    }
+}
+
+/* ============================================ Blockchain Class ================================================================
+|  -Class with a constructor for new blockchain 		                                                                              | 
+|   -Constructor is in charge of wether a genesis block should created or generate a new block from the last block in the database|
+|  ==============================================================================================================================*/
+
+class Blockchain{
+  constructor(){
+    // construct chain in memory 
+    this.chain = [];
+    // get height of db, this allow to add block to db without deletion of chain
+    db.getBlocksCount().then((result) => {
+      // If nothing create a genesis block
+      if(!result) {
+        let GenBlock = new Block("First block in the chain - Genesis block");
+        GenBlock.time = new Date().getTime().toString().slice(0,-3);
+        GenBlock.height = 0;
+        GenBlock.hash = SHA256(JSON.stringify(GenBlock)).toString();
+        this.chain.push(GenBlock);
+        db.addDataToLevelDB(JSON.stringify(GenBlock).toString());
+        // else create a new block with the correct height 
+        // NOTE this means a new block will be add when constucting Blockchain class
+        }else {
+          db.getLevelDBData(result - 1).then((resultBlock) => {
+            if(!resultBlock){
+              console.log('Cant ID last DB block');
+              //create new block with the height + 1 of last block added to db
+              // the new block is add to database chain and memory chain
+            }else{
+              var lastDBblock = JSON.parse(resultBlock);
+              var PH = lastDBblock.hash;
+              let CB = new Block();
+              CB.previousBlockHash = PH;
+              CB.height = result;
+              CB.time = new Date().getTime().toString().slice(0,-3);
+              CB.hash = SHA256(JSON.stringify(CB)).toString();
+              // Chain in memory
+              this.chain.push(CB);
+              // Chain in db 
+              db.addDataToLevelDB(JSON.stringify(CB).toString());
+              
+            }
+           }).catch((err) => { console.log(err); });
+          
+        }
+    }).catch((err) => { console.log(err); });
+    
+  }
+
+
+/* ==================== Create new block =================
+|    Function within Blockchain class to add new blocks  |
+|  =====================================================*/
+
+  addBlock(newBlock){
+    // Block height from chain in memory
+    newBlock.height = this.chain[this.chain.length - 1].height + 1;
+    // UTC timestamp
+    newBlock.time = new Date().getTime().toString().slice(0,-3);
+    // previous block hash
+    if(newBlock.height>0){
+      // Will have to change for no prior hash value aka starting from ! 0 
+      newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
+    }
+    // Block hash with SHA256 using newBlock and converting to a string
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+    // Adding block object to chain
+    db.addDataToLevelDB(JSON.stringify(newBlock).toString());
+    // Add block to chain in memory
+  	this.chain.push(newBlock);
+  }
+
+
+/* ======================= Validate a single block =========================
+|   -Function within Blockchain class to validate a single block.           |
+|    -Block is rehashed and compared to validate the block .                |
+|    -When rehashing the block the hash value is remove to get a valid hash.|
+|    INPUT                                                                  |
+|      intager (block height)                                               |
+|    OUTPUT                                                                 |
+|      string (Block vaildation) as a promise                               |
+|  ========================================================================*/
+
+      validateBlock(blockHeight){
+        return new Promise(resolve => {
+        // get requested block 
+        db.getLevelDBData(blockHeight).then((block) => {
+          if (!block){
+            console.log('Error With getting Block within validateBlock')
+            resolve("A Error Accored, please check block range");
+          }else{
+            // Turn block into object
+            var obj = JSON.parse(block);
+            // Hold block hash
+            var blockhash = obj.hash;
+            // make block hash empty sting 
+            obj.hash = "";
+            // rehash the block 
+            var validBlockHash = SHA256(JSON.stringify(obj)).toString();
+            // put back hash for validation
+            obj.hash = blockhash;
+            // compare hashes and validate the block
+            if (obj.hash === validBlockHash){
+              var CorrectString = ("Block #"+obj.height+" is validated and intacted")
+              console.log('\x1b[32m%s\x1b[0m',CorrectString);
+              resolve("Valid");
+            }else{
+              console.log('\x1b[31m%s\x1b[0m',"WARNING block #"+obj.height+" has been tampered with or corrupt. Please check chain integerite using validateChain()");
+              resolve("Invalid");
+            }
+      }
+    });
+  });
+  }
+
+
+/* ================================================ Validate the entire block chain ================================================
+|   -Function within Blockchain class to validate the block chain                                                                     |
+|    -This function uses the power of promise to resolve the itegerate of the chain                                                   |
+|        -Loop through each block                                                                                                     |
+|          -If block is invalid a resolve is returned                                                                                 |
+|          -Else do nothing until the end of the chain. If a resolve is not returned before the end of the chain, resolve chain vaild.|
+|    INPUT                                                                                                                            |
+|        NONE                                                                                                                         |
+|    OUTPUT                                                                                                                           |
+|        string that discribes the chain is valid or not as a promise                                                                 |
+|  ================================================================================================================================*/
+
+    validateChain(){
+      return new Promise(resolve => {
+        //get the height of the chain 
+      db.getBlocksCount().then((result) => {
+        if(!result) {
+          console.log('Error with getting chain height within validateChain');
+          }else {
+            var CorrectCounter = 0;
+            // loop through each block from block one used to compare pervious hash
+            for (var a = 0; a < result + 1; a++){
+              // get blocks value used to compare 
+              // NOTE Block objects are returns as strings
+              db.getLevelDBData(a-1).then((hash) => {
+                if (!hash){
+                  console.log('Error With getting block within validateChain')
+                }else{
+                  //Turn string back into block object
+                  var obj = JSON.parse(hash);
+                  // Hold block hash for comparing
+                  var blockhash = obj.hash;
+                  // remove hash from block object
+                  obj.hash = "";
+                  // Hash block with SHA256 to compare with hash in block
+                  var validBlockHash = SHA256(JSON.stringify(obj)).toString();
+                  // put back object hash that was removed
+                  obj.hash = blockhash;
+                  //genesis block has no pervious hash, block is only compared to the hash done to the block 
+                  if (obj.height == 0){
+                    // Comparing the hash we just created (validBlockHash) with the hash in the block
+                    if (obj.hash === validBlockHash){
+                      console.log("genesis Block Valid")
+                    }else{
+                      console.log("genesis block invalid")
+                      // if genesis is invalid we resolve the promise with string and block height
+                      var StringEnder = "Chain invalid, please check block "+obj.height;
+                      resolve(StringEnder);
+                    }
+                  }else{
+                  // if not genesis block, get the pevious block by subtracting one from the height 
+                  db.getLevelDBData(obj.height - 1).then((priorHash) => {
+                    if (!priorHash){
+                      console.log("Error getting next Block")
+                      }else{
+                        // make the previous block into a object 
+                        var Newobj = JSON.parse(priorHash);
+                        // Full vaildation of the block:
+                        // 1.Compare main block hash and next block previous hash
+                        // 2.Compare Hash just create and hash already in the block 
+                        if (Newobj.hash === obj.previousBlockHash && validBlockHash === obj.hash){
+                          // Reach the end of the chain and resolve the promise with a vaild string
+                          if (result - 2 == CorrectCounter){
+                            resolve("CHAIN VALID")
+                          }
+                          // counter for chain position
+                          CorrectCounter += 1;
+                        // if block or previous block is invaild, resolve promise with string with blocks that are invalid
+                        }else{
+                          console.log("Check prior hash match with blocks:",obj.height, "&",obj.height -1  )
+                          var prior = obj.height - 1
+                          var StringEnder = "CHAIN invalid, please check blocks "+obj.height+" and "+prior;
+                          resolve(StringEnder);
+                        }
+                      }
+                  });
+                }
+                }
+              }).catch((err) => { console.log(err);});
+            }
+          }
+          
+      }).catch((err) => { console.log(err); });
+      
+    })
+  }
+
+/* ==================== Get the height of the block chain =======================
+|  -Function within Blockchain class to get the prior height of the block chain  |
+|  -Uses getBlockCount to height of the chain                                    |
+|   INPUT                                                                        |
+|        NONE                                                                    |
+|    OUTPUT                                                                      |
+|        intager of height of chain as a promise                                 |
+|  ==============================================================================*/
+
+    getHeight(){
+      return new Promise(resolve => {
+        db.getBlocksCount().then((result) => {
+          if (!result){
+            console.log('Error With getting hash')
+          }else{
+            resolve(result);
+          }
+        });
+      });
+    }
+
+  
+/* ============= Get a single block of the block chain ===================
+|   -Function within Blockchain class to get a speific block on the chain |
+|    -Uses getLevelDBData to reateive block from leveldb                  |
+|    INPUT                                                                |
+|        intager used to get specific block                               | 
+|    OUTPUT                                                               |
+|        block object as a promise                                        |
+|  ======================================================================*/
+
+    getBlock(BlockN){
+      return new Promise(resolve => {
+        db.getLevelDBData(BlockN).then((result) => {
+          if(!result){
+          }else{
+            resolve(result);
+          }
+        })
+      })
+    }
+
+
+/* ================= Get the entire block chain ====================
+|   -Function within Blockchain class to get the entire block chain |
+|   -Used to visualize the block chain on the clients browser       |
+|   -Uses db.getChain() to get chain                                |
+|   INPUT                                                           |
+|        None                                                       |
+|   OUTPUT                                                          |
+|        list of block objects as a promise                         |
+|  =================================================================*/
+
+    ChainRecon(){
+      return new Promise(resolve => {
+        db.getChain().then((chain) => {
+          if (!chain){
+            console.log('Error With getting Chain')
+            resolve("Cant sync chain");
+          }else{
+            resolve(chain);
+          }
+        });
+      });
+  }
+}
+
+
+// Create blockchain object
+const PrivateChain = new Blockchain;
+
+/* ========================== Creating blocks  ====================================
+|  -Allows for the creation of new blocks                                         |
+|  -Using addBlock function with in blockchain class                              |
+| INPUTS                                                                          |
+|  -Taking in two intagers (timing,amount).                                       |
+|  -Timing specifying the timeout period between blocks being created.            | 
+|  -Amount of blocks to be created.                                               |
+| OUTPUTS                                                                         |
+|   returns a string as a promise                                                 |
+|  ==============================================================================*/
+
+
+async function BlockCreator(timing,amount){
+  return new Promise(resolve => {
+  (function theLoop (i) {
+    setTimeout(function () {
+        const BlockModel = new Block;
+        PrivateChain.addBlock(BlockModel);
+          i++;
+      if (i < amount - 1) { 
+            theLoop(i) 
+          } else {
+            resolve("Blocks Added Successful");
+          }
+    }, timing);
+    })(0);
+  });
+  }
+
+
+
+
+/* =============================================== Web Server  ==================================================
+|   Below is a functional web server, that clients can post and get data directly through the block chain class |
+|  =============================================================================================================*/
+
+
+//Note that in version 4 of express, express.bodyParser() was
+//deprecated in favor of a separate 'body-parser' module.
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// get request made by client to get a single block
+// Uses getBlock within blockchain class
+app.get('/singleBlock',async function(req,res){
+
+    var BlockNumber = req.query.SingleB;
+    var x = await PrivateChain.getBlock(BlockNumber);
+    res.setHeader('Content-Type','text/json',);
+    res.json({'Block': x});
+    res.end();
+
+});
+
+// get request made by client to vaildate block
+// Uses validateBlock within blockchain class
+app.get('/blocks',async function(req,res){
+
+    var BlockNumber = req.query.Block;
+    var x = await PrivateChain.validateBlock(BlockNumber);
+    res.setHeader('Content-Type','text/json',);
+    res.json({'Block': x});
+    res.end();  
+
+});
+
+// get request made by client to get chain only for visual
+// uses ChainRecon within BlockChain Class
+app.get('/chain',async function(req,res){
+
+  var CC = await PrivateChain.ChainRecon();
+  res.setHeader('Content-Type','text/json',);
+  res.json({'chain': CC});
+  res.end();
+
+});
+
+// get request made by client to validate chain
+// uses validateChain within BlockChain class
+app.get('/valchain',async function(req,res){
+
+  var VC = await PrivateChain.validateChain();
+  res.setHeader('Content-Type','text/json',);
+  res.json({'chain': VC});
+  res.end();
+
+});
+
+//get request made by client to get height of chain
+// uses getHeight within BlockChain Class
+app.get('/height',async function(req,res){
+
+  var H = await PrivateChain.getHeight();
+  res.setHeader('Content-Type','text/json',);
+  res.json({'Height': H});
+  res.end();
+
+});
+
+// post request make by client to add blocks to the chain
+// uses Blockcreator to create more blocks
+app.post('/addblock',async function(req,res) {
+
+  var Response = BlockCreator(req.body.timing,req.body.amount)
+  res.setHeader('Content-Type','text/json',);
+  res.json({'BlockStatus': Response});
+  res.end();
+  
+});
+
+// post request make by client to change blocks 
+// Using changeDBData a development only tool 
+app.post('/Change',async function(req,res) {
+
+  var key = req.body.height;
+  var value = JSON.stringify(req.body).toString();
+  var Response = db.changeDBData(key,value);
+  res.setHeader('Content-Type','text/json',);
+  res.json({'BlockStatus': Response});
+  res.end();
+
+})
+
+// Get the html file 
+fs.readFile('./privateblockchainUI.html', function (err, html) {
+app.get('/',function(req,res){
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(html);  
+  res.end();
+
+  });
+});
+// start the web server
+app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
+});
